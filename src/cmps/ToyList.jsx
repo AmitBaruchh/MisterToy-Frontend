3d565477efe2a4f425e55c21da8ad92b79bafae7
Amit Baruch <amitbaruch08@gmail.com>
2024-11-01 13:26:59 +0200

    Add ToyList component with edit and remove options

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -1,3 +1,22 @@
-export function ToyList() {
-    return <div>Toy List</div>
+import { Link } from 'react-router-dom'
+import { ToyPreview } from './ToyPreview'
+
+export function ToyList({ toys, onRemoveToy }) {
+    return (
+        <section className="toy-list">
+            <ul>
+                {toys.map(toy => (
+                    <li key={toy._id}>
+                        <ToyPreview toy={toy} />
+                        <div className="btn-container">
+                            <button>
+                                <Link to={`/toy/edit/${toy._id}`}>Edit</Link>
+                            </button>
+                            <button onClick={() => onRemoveToy(toy._id)}>Remove</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
 }
