5da4b953a8ee6c95bfd92ca862eb9e591d9860dc
Amit Baruch <amitbaruch08@gmail.com>
2024-11-01 11:17:27 +0200

    Create services folder

---
+++ src/services/toy.service.js
@@ -0,0 +1,10 @@
+const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle', 'Outdoor', 'Battery Powered']
+
+const toy = {
+    _id: 't101',
+    name: 'Talking Doll',
+    price: 123,
+    labels: ['Doll', 'Battery Powered', 'Baby'],
+    createdAt: 1631031801011,
+    inStock: true,
+}
